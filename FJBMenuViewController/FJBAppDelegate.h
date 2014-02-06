221cb9eedf758ae9753aab8d03538f4376d0050c
Fran <fjbelchi@gmail.com>
2014-02-06 12:51:03 +0100

    Initial Commit

---
+++ FJBMenuViewController/FJBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FJBAppDelegate.h
+//  FJBMenuViewController
+//
+//  Created by Fran_DEV on 06/02/14.
+//  Copyright (c) 2014 FJBelchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FJBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
